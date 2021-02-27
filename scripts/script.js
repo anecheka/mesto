97c6ce51aaade5fcad02140d47178d7c910dfc8b
Anya Ch <annnacha@yandex.ru>
2021-02-28 02:17:06 +0300

    addressing comments from first review

--- scripts/script.js
+++ scripts/script.js
@@ -1,46 +1,45 @@
-let clickEditButton = document.querySelector('#edit-profile'); 
-let popup = document.querySelector('.popup'); 
-let newUsername = document.querySelector('#username'); 
-let newBio = document.querySelector('#bio'); 
-let currentUsername = document.querySelector('.profile__username');
-let currentBio = document.querySelector('.profile__bio');
-let closePopup = document.querySelector('#close-popup'); 
-let form = document.querySelector('.form');
-let submitInfo = document.querySelector('#submit-profile-info')
+let clickEditButton = document.querySelector('#edit-profile'); //Переменная для кнопки редактирования профиля
+let popup = document.querySelector('.popup'); //Переменная для попапа 
+let newUsername = document.querySelector('#username'); //Переменная для инпута с именем пользователя
+let newBio = document.querySelector('#bio'); //Переменная для инпута с биографией
+let currentUsername = document.querySelector('.profile__username'); //Переменная с именем пользователя в профиле на странице 
+let currentBio = document.querySelector('.profile__bio'); //Переменная с био пользователя в профиле на странице
+let closePopup = document.querySelector('#close-popup'); //Переменная для кнопки-крестика
+let form = document.querySelector('.form'); //Переменная для формы
 
-function clickClose () {
+//Функция закрытия попапа
+function clickClose() {
     popup.classList.remove('popup_is-opened');
 };
 
+//Функция сохранения информации из попапа в профиль пользователя (редактирование профиля)
 function editProfile() {
     currentUsername.textContent = newUsername.value;
     currentBio.textContent = newBio.value;
 };
 
-function showPopup () {
+//Функция открытия попапа
+function showPopup() {
     popup.classList.add('popup_is-opened');
     newUsername.value = currentUsername.textContent;
     newBio.value = currentBio.textContent;
 };
 
-submitInfo.addEventListener ('keyup', function(event) {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        submitInfo.click();
-    }
-});
-
-clickEditButton.addEventListener ('click', showPopup);
-
-closePopup.addEventListener ('click', clickClose);
-
-form.addEventListener ('submit', evt => {
+//Функция передачи данных из попапа с формой на страницу 
+function submitInfo(evt) {
     evt.preventDefault();
     editProfile();
     clickClose();
-    console.log('triggered');
-});
+};
 
+//Вызываем функцию открытия попапа при клике на иконку редактирования профиля
+clickEditButton.addEventListener('click', showPopup);
+
+//Вызываем функцию закрытия попапа при клике на иконку крестика 
+closePopup.addEventListener('click', clickClose);
+
+//Сохраняем данные, введенные в попапе, в профиль пользователя на странице, и закрываем попап
+form.addEventListener('submit', submitInfo);
 
 
 
